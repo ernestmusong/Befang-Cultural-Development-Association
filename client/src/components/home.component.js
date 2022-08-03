8af87493c2c5b85eafaca36e0d0bea440f97e64b
Musong Ernest Akeh <ernestakeh@gmail.com>
2022-08-03 13:39:19 +0100

    some change

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -39,7 +39,7 @@ export default class Home extends Component {
   render() {
     return (
       <>
-      <div className="banner mx-auto" style={{ backgroundImage: `url(${banner})` }}>
+      <div className="banner mx-auto" style={{ backgroundImage: `url(${banner})`}}>
         <div className="text1-wrapper " style={{margin: "0 auto", marginBottom:"4rem", paddingTop:"4rem" }}>
             <h1 className="text1" >welcome to becuda</h1>
         </div>
