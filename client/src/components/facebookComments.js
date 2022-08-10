fa862ee2a98aaf2a8b27b395244debc1be65c4f4
Musong Ernest Akeh <ernestakeh@gmail.com>
2022-08-10 15:32:17 +0100

    sa

--- client/src/components/facebookComments.js
+++ client/src/components/facebookComments.js
@@ -1,24 +1,16 @@
 import React from 'react'
-import {useState, useEffect} from 'react'
+import { useEffect} from 'react'
 import { facebookComments} from '../db.json'
-import { useHistory } from 'react-router-dom'
 
 function Comments() {
-    const[comments, setComments] = useState({
-        dataHref: "",
-        dataWidth: "",
-        dataNumposts: "",
-        dataColorScheme: ""
-       })
-       let history = useHistory() 
-       
+    
+    let comments = facebookComments
        useEffect(() =>{
-        let comments = facebookComments
-         setComments(comments)
-         console.log(comments)
-       }, [history]) 
-       console.log(history)
-        console.log(history.location.pathname)
+         if (window.FB) {
+            window.FB.XFBML.parse();
+        }
+       }, []) 
+        
   return (
     <div className='bg-light'>
     <div class="fb-comments" data-href={comments.dataHref} data-width={comments.dataWidth} data-numposts={comments.dataNumposts} data-colorscheme={comments.dataColorScheme}></div>
